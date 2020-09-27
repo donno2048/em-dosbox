1859ace914b1547b5ae253bf0353d0066a645a93
Bobby Lockwood <ulsterbobby@gmail.com>
2020-09-27 13:44:43 +0100

    Add more to the new testing framework

--- tests/app.js
+++ tests/app.js
@@ -3,7 +3,11 @@
 const DELAY = process.env.DELAY || 5000;
 
 const commandLineArgs = require('command-line-args');
-const { w3cwebsocket } = require('websocket');
+const Express = require('express');
+var app = Express();
+/*const WebSocket = */require('express-ws')(app);
+const fs = require('fs');
+const Browser = require('puppeteer');
 
 const usage = commandLineArgs([
     {
@@ -16,16 +20,19 @@ const usage = commandLineArgs([
         name: 'port', type: Number, description: 'Port, affected by Environment Variable Port'
     },
     {
-        name: 'output', alias: 'o', type: String, description: 'Output to directory [can only be used with --run'
+        name: 'output', alias: 'o', type: String, description: 'Output to directory [can only be used with --run]'
     },
     {
         name: 'run', alias: 'r', type: Boolean, description: 'Run Tests'
+    },
+    {
+        name: 'headed', alias: 'd', type: Boolean, description: 'Headed [usually test automation is headless]'
     }
 ]);
 
 if (process.env.PORT) {
 
-    usage.port = process.env.PORT;
+    usage.port = Number.parseInt(process.env.PORT);
 
 } else if (!usage.port) {
 
@@ -35,55 +42,95 @@ if (process.env.PORT) {
 
 //Run Web Server
 //Load HTTP Server and WebSocket Server
-App = Express();
-HttpServer = App.listen(usage.port);
-WsServer = new w3cwebsocket({ httpServer: HttpServer });
 
-//Proxy
-WsServer.onerror = function (err) {
-    console.error(err);
-}
+var httpServer = app.listen(usage.port);
 
-WsServer.onopen = function () {
-    console.log("Handle proxy events");
-}
+app.ws('/', function (ws, req) {
+    ws.on('error', function (msg) {
+        ws.send(msg);
+    });
+    ws.on('message', function (msg) {
+        ws.send(msg);
+    });
+
+    console.log(req);
+});
 
 //Load Static File Server @ Path
-App.use(Express.static(usage.path));
+app.use(Express.static(usage.path));
 
-var stdout = "";
-var stderr = "";
-
-//Grab output
-Browser.extend(function (browser) {
-    browser.on('console', function (level, message) {
-        console.log(level, message);
-    });
-});
+var std = "";
 
 //If run
 if (usage.run) {
-    const Browser = require('zombie');
 
-    //Browse to localhost:$PORT
-    Browser.localhost('localhost', usage.port);
+    function iserr(err) {
+        if (err) {
+            console.error(err);
+        }
+    }
 
-    describe('Application is launched', function () {
+    function timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-        var browser = new Browser();
+    async function sleep(ms, fn, ...args) {
+        await timeout(ms);
+        return fn(...args);
+    }
 
-        before(function () {
-            return browser.visit(usage.url);
+    async function capture(instance, page) {
+        console.log("Capturing result"); //Note Windows has supported '/' in path since 2.0
+        var oScreenshot = (usage.output == undefined ? (usage.path + '/screenshot.png') : (usage.output + '/screenshot.png'));
+        var oStd = (usage.output == undefined ? (usage.path + '/std.txt') : (usage.output + '/std.txt'));
+
+        await page.screenshot({ path: oScreenshot });
+        fs.writeFile(oStd, std, iserr);
+
+        console.log("Closing instance");
+        await instance.close();
+
+        console.log("Returning to main thread");
+        return;
+    }
+
+    async function run() {
+
+        console.log("Launching Instance");
+        const instance = (usage.headed ? await Browser.launch({ headless: false }) : await Browser.launch());
+
+        console.log("Opening Page");
+        const page = await instance.newPage();
+        page.setJavaScriptEnabled = true;
+
+        page.on('console', msg => std += msg);
+
+        await page.goto(`http://localhost:${usage.port}/${usage.url}`);
+
+        //Wait for the program to run a bit
+        const dimensions = await page.evaluate(() => {
+            return {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight,
+                deviceScaleFactor: window.devicePixelRatio
+            };
         });
 
-        setTimeout(function () {
+        console.log(dimensions);
 
-            
+        //Sleep for 10 seconds, then capture with instance of browser on page x
+        sleep(10000, capture, instance, page);
 
-        }, DELAY);
+    };
 
-    })
+    new Promise(run).then(() => {
 
+        console.log("done");
 
+    }).catch((reason) => {
+
+        console.error(reason);
+
+    });
 
 }
