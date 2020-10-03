de3d15006ebc50ba297a0a97bf388675d7dba4e5
Bobby Lockwood <ulsterbobby@gmail.com>
2020-10-03 16:05:26 +0100

    Finish New Packager

--- packager/template.js
+++ packager/template.js
@@ -1,185 +1,194 @@
-//var Module = typeof Module !== 'undefined' ? Module : {};
+module.exports = (usage, directories, files) => `
 
-//if (!Module.expectedDataFileDownloads) {
-//    Module.expectedDataFileDownloads = 0;
-//}
+var Module = typeof Module !== "undefined" ? Module : {
+};
 
-//Module.expectedDataFileDownloads++;
+if (!Module.expectedDataFileDownloads) {
+    Module.expectedDataFileDownloads = 0;
+}
 
-//function DataRequest(start, end) {
-//    this.start = start;
-//    this.end = end;
-//}
+Module.expectedDataFileDownloads++;
 
-//DataRequest.prototype = {
-//    requests: {},
+function DataRequest(start, end) {
+    this.start = start;
+    this.end = end;
+}
 
-//    open: function (mode, name) {
-//        this.name = name;
-//        this.requests[name] = this;
-//        Module['addRunDependency']('fp ' + this.name);
-//    },
+DataRequest.prototype = {
+    requests: {},
 
-//    send: function () { },
+    open: function (mode, name) {
+        this.name = name;
+        this.requests[name] = this;
+        Module.addRunDependency("fp " + this.name);
+    },
 
-//    onload: function () {
-//        var byteArray = this.byteArray.subarray(this.start, this.end);
-//        this.finish(byteArray);
-//    },
+    send: function () { console.log("DataRequest send: Not Implemented"); },
 
-//    finish: function (byteArray) {
-//        var that = this;
+    onload: function () {
+        var byteArray = this.byteArray.subarray(this.start, this.end);
+        this.finish(byteArray);
+    },
 
-//        Module['FS_createDataFile'](this.name, null, byteArray, true, true, true); // canOwn this data in the filesystem, it is a slide into the heap that will never change
-//        Module['removeRunDependency']('fp ' + that.name);
+    finish: function (byteArray) {
+        var that = this;
 
-//        this.requests[this.name] = null;
-//    }
-//};
+        // canOwn this data in the filesystem, it is a slide into the heap that will never change
+        Module.FS_createDataFile(this.name, null, byteArray, true, true, true);
+        Module.removeRunDependency("fp " + that.name);
 
-//function assert(check, msg) {
-//    if (!check) throw msg + new Error().stack;
-//}
+        this.requests[this.name] = null;
+    }
+};
 
-//function handleError(error) {
-//    console.error('package error:', error);
-//}
+function assert(check, msg) {
+    if (!check) {
+        throw msg + new Error().stack;
+    }
+}
 
-//function fetchRemotePackage(packageName, packageSize, callback, errback) {
-//    var xhr = new XMLHttpRequest();
-//    xhr.open('GET', packageName, true);
-//    xhr.responseType = 'arraybuffer';
+function handleError(error) {
+    console.error("package error:", error);
+}
 
-//    xhr.onprogress = function (event) {
-//        var url = packageName;
-//        var size = packageSize;
-//        if (event.total) size = event.total;
-//        if (event.loaded) {
-//            if (!xhr.addedTotal) {
-//                xhr.addedTotal = true;
-//                if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
-//                Module.dataFileDownloads[url] = {
-//                    loaded: event.loaded,
-//                    total: size
-//                };
-//            } else {
-//                Module.dataFileDownloads[url].loaded = event.loaded;
-//            }
-//            var total = 0;
-//            var loaded = 0;
-//            var num = 0;
-//            for (var download in Module.dataFileDownloads) {
-//                var data = Module.dataFileDownloads[download];
-//                total += data.total;
-//                loaded += data.loaded;
-//                num++;
-//            }
-//            total = Math.ceil(total * Module.expectedDataFileDownloads / num);
-//            if (Module['setStatus']) Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
-//        } else if (!Module.dataFileDownloads) {
-//            if (Module['setStatus']) Module['setStatus']('Downloading data...');
-//        }
-//    };
-//    xhr.onerror = function (event) {
-//        throw new Error("NetworkError for: " + packageName);
-//    }
-//    xhr.onload = function (event) {
-//        if (xhr.status == 200 || xhr.status == 304 || xhr.status == 206 || (xhr.status == 0 && xhr.response)) { // file URLs can return 0
-//            var packageData = xhr.response;
-//            callback(packageData);
-//        } else {
-//            throw new Error(xhr.statusText + " : " + xhr.responseURL);
-//        }
-//    };
-//    xhr.send(null);
-//}
+function fetchRemotePackage(packageName, packageSize, callback, errback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", packageName, true);
+    xhr.responseType = "arraybuffer";
 
-//function processPackageData(arrayBuffer) {
-//    assert(arrayBuffer, 'Loading data file failed.');
-//    assert(arrayBuffer instanceof ArrayBuffer, 'bad input to processPackageData');
-//    var byteArray = new Uint8Array(arrayBuffer);
+    xhr.onprogress = function (event) {
+        var url = packageName;
+        var size = packageSize;
+        if (event.total) size = event.total;
+        if (event.loaded) {
+            if (!xhr.addedTotal) {
+                xhr.addedTotal = true;
+                if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
+                Module.dataFileDownloads[url] = {
+                    loaded: event.loaded,
+                    total: size
+                };
+            } else {
+                Module.dataFileDownloads[url].loaded = event.loaded;
+            }
+            var total = 0;
+            var loaded = 0;
+            var num = 0;
+            for (var download in Module.dataFileDownloads) {
+                if (Module.dataFileDownloads.hasOwnProperty(download)) {
+                    var data = Module.dataFileDownloads[download];
+                    total += data.total;
+                    loaded += data.loaded;
+                    num++;
+                }
+            }
+            total = Math.ceil(total * Module.expectedDataFileDownloads / num);
+            if (Module.setStatus) Module.setStatus("Downloading data... (" + loaded + '/' + total + ")");
+        } else if (!Module.dataFileDownloads) {
+            if (Module.setStatus) Module.setStatus("Downloading data...");
+        }
+    };
+    xhr.onerror = function (event) {
+        throw new Error("NetworkError for: " + packageName);
+    };
+    xhr.onload = function (event) {
+        if (xhr.status == 200 || xhr.status == 304 || xhr.status == 206 || (xhr.status == 0 && xhr.response)) { // file URLs can return 0
+            var packageData = xhr.response;
+            callback(packageData);
+        } else {
+            throw new Error(xhr.statusText + " : " + xhr.responseURL);
+        }
+    };
+    xhr.send(null);
+}
 
-//    // Reuse the bytearray from the XHR as the source for file reads.
-//    DataRequest.prototype.byteArray = byteArray;
+function processPackageData(arrayBuffer, metadata) {
+    assert(arrayBuffer, "Loading data file failed.");
+    assert(arrayBuffer instanceof ArrayBuffer, "bad input to processPackageData");
+    var byteArray = new Uint8Array(arrayBuffer);
 
-//    var files = metadata['files'];
-//    for (var i = 0; i < files.length; ++i) {
-//        DataRequest.prototype.requests[files[i].filename].onload();
-//    }
+    // Reuse the bytearray from the XHR as the source for file reads.
+    DataRequest.prototype.byteArray = byteArray;
 
-//    Module['removeRunDependency']('datafile_${packageName}');
+    var files = metadata.files;
+    for (var i = 0; i < files.length; ++i) {
+        DataRequest.prototype.requests[files[i].filename].onload();
+    }
 
-//};
+    Module.removeRunDependency("datafile_\`\${packageName}\`");
 
+}
 
-//function loadPackage(metadata) {
-//    var packagePath;
+function loadPackage(metadata) {
+    var packagePath;
 
-//    if (typeof window === 'object') {
-//        packagePath = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
-//    } else if (typeof location !== undefined) {
-//        packagePath = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
-//    } else {
-//        throw "Using preloaded data can only be done on a web page or in a web worker";
-//    }
+    if (typeof window === "object") {
+        packagePath = window.encodeURIComponent(window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
+    } else if (typeof location !== undefined) {
+        packagePath = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
+    } else {
+        throw "Using preloaded data can only be done on a web page or in a web worker";
+    }
 
-//    var packageName = "${usage.output}.data"
+    var packageName = "${usage.output}.data";
 
-//    if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
-//        Module['locateFile'] = Module['locateFilePackage'];
-//        err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
-//    }
+    if (typeof Module.locateFilePackage === "function" && !Module.locateFile) {
+        Module.locateFile = Module.locateFilePackage;
+        handleError('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
+    }
 
-//    var packageSize = metadata['remote_package_size'];
+    var packageSize = metadata.remote_package_size;
 
-//    var fetchedCallback = null;
-//    var fetched = Module['getPreloadedPackage'] ? Module['getPreloadedPackage'](packageName, packageSize) : null;
+    var fetchedCallback = null;
+    var fetched = Module.getPreloadedPackage ? Module.getPreloadedPackage(packageName, packageSize) : null;
 
-//    if (!fetched) {
-//        fetchRemotePackage(packageName, packageSize, function (data) {
-//            if (fetchedCallback) {
-//                fetchedCallback(data);
-//                fetchedCallback = null;
-//            } else {
-//                fetched = data;
-//            }
-//        }, handleError);
-//    }
+    if (!fetched) {
+        fetchRemotePackage(packageName, packageSize, function (data) {
+            if (fetchedCallback) {
+                fetchedCallback(data, metadata);
+                fetchedCallback = null;
+            } else {
+                fetched = data;
+            }
+        }, handleError);
+    }
 
-//    function runWithFS() {
+    function runWithFS() {
 
-//        ${ jsDirectories }
+        ${directories}
 
-//        var files = metadata['files'];
-//        for (var i = 0; i < files.length; ++i) {
-//            new DataRequest(files[i]['start'], files[i]['end'], files[i]['audio']).open('GET', files[i]['filename']);
-//        }
+        var files = metadata.files;
+        for (var i = 0; i < files.length; ++i) {
+            new DataRequest(files[i].start, files[i].end, files[i].audio).open("GET", files[i].filename);
+        }
 
-//        Module['addRunDependency']('datafile_${packageName}');
+        Module.addRunDependency('datafile_\`\${packageName}\`');
 
-//        if (!Module.preloadResults) Module.preloadResults = {};
+        if (!Module.preloadResults) Module.preloadResults = {};
 
-//        Module.preloadResults[packageName] = { fromCache: false };
+        Module.preloadResults[packageName] = { fromCache: false };
 
-//        if (fetched) {
-//            processPackageData(fetched);
-//            fetched = null;
-//        } else {
-//            fetchedCallback = processPackageData;
-//        }
-//    }
+        if (fetched) {
+            processPackageData(fetched, metadata);
+            fetched = null;
+        } else {
+            fetchedCallback = processPackageData;
+        }
+    }
 
-//    if (Module['calledRun']) {
-//        runWithFS();
-//    } else {
-//        if (!Module['preRun']) Module['preRun'] = [];
-//        Module["preRun"].push(runWithFS); // FS is not initialized yet, wait for it
-//    }
+    if (Module.calledRun) {
+        runWithFS();
+    } else {
+        if (!Module.preRun) Module.preRun = [];
+        Module.preRun.push(runWithFS); // FS is not initialized yet, wait for it
+    }
 
-//}
+}
 
-//(function () {
-//    loadPackage({ "files": ${fileToLoad }})
-//})();
+(function () {
+    loadPackage({ "files": ${JSON.stringify(files)} });
+})();
 
-//Module['arguments'] = ['./BIN/QBX.EXE'];
+Module.arguments = ["${usage.run}"];
+
+`
