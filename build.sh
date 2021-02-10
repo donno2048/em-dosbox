0d07c622447b10de51dde0d02fa369b743916954
Elisha Hollander <just4now666666@gmail.com>
2021-02-10 05:51:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ cd ..
 sudo apt install libc6-dev g++ gcc -y
 sudo autoreconf -f -i
 ./autogen.sh
-emconfigure ./configure --enable-wasm --disable-opengl --host=none-none-none AR=/usr/bin/ar CC=gcc
-make
+./configure --enable-wasm --disable-opengl --host=none-none-none AR=/usr/bin/ar CC=gcc
+emmake make
 cd src
 python packager.py win95 win AUTOEXEC.BAT
 sed -i -e 's/(simulateInfiniteLoop)/(false)/g' dosbox.js
